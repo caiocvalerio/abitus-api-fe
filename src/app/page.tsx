d96b7fb43a3aedbe22a70e8f57b9e90f9e463612
Caio C L Valerio <caiocvalerio@gmail.com>
2025-09-05 19:14:29 -0400

    Tentativa de subir projeto no vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use server"; // tentativa de subir projeto no vercel
+
 import { getPessoas } from "@/services/pessoaService";
 import HomePageClient from "./_components/HomePageClient";
 import { JSX } from "react";
+import { PagePessoa } from "@/types";
 
 export default async function Home(): Promise<JSX.Element> {
-  const initialPageData = await getPessoas({ pagina: 0, porPagina: 12 });
+  const initialPageData: PagePessoa = await getPessoas({ pagina: 0, porPagina: 12 });
 
   return (
     <HomePageClient initialPageData={initialPageData} />
